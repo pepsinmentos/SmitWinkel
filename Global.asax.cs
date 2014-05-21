5599d08c8622ab68d3f339a937c7ffe6c126facb
bcodendaal <bcodendaal@gmail.com>
2014-05-21 23:03:12 +0200

    added bootstrap

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
+//using System.Data.Entity.Infrastructure;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
@@ -40,8 +40,8 @@ namespace SmitWinkel
         {
             AreaRegistration.RegisterAllAreas();
 
-            // Use LocalDB for Entity Framework by default
-            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
+            //// Use LocalDB for Entity Framework by default
+            //Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
